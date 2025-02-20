fa3f7c5def9a46bfdc14649711313fa446b0f2af
Temitope okikiolu Bello <temmytopeqa@gmail.com>
2025-02-20 01:35:35 +0100

    frozenset

--- datatypesset.py
+++ datatypesset.py
@@ -5,3 +5,19 @@ print(x)
 
 #display the data type of x:
 print(type(x))
+
+x = {"apple", "banana", "cherry"}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
