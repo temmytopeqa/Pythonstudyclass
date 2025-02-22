14007de8800cd2edf45d46a4d6c3474922aeeaa7
Temitope okikiolu Bello <temmytopeqa@gmail.com>
2025-02-22 10:57:40 +0100

    Stringmethod

--- Pythnumbers.py
+++ Pythnumbers.py
@@ -129,4 +129,204 @@ print(txt)
 txt = "\x48\x65\x6c\x6c\x6f"
 print(txt)
 
+txt = "hello, and welcome to my world."
+
+x = txt.capitalize()
+
+print (x)
+
+txt = "python is FUN!"
+
+x = txt.capitalize()
+
+print (x)
+
+txt = "36 is my age."
+
+x = txt.capitalize()
+
+print (x)
+
+txt = "Hello, And Welcome To My World!"
+
+x = txt.casefold()
+
+print(x)
+
+txt = "banana"
+
+x = txt.center(20)
+
+print(x)
+
+txt = "I love apples, apple are my favorite fruit"
+
+x = txt.count("apple")
+
+print(x)
+
+txt = "My name is Ståle"
+
+x = txt.encode()
+
+print(x)
+
+txt = "Hello, welcome to my world."
+
+x = txt.endswith(".")
+
+print(x)
+
+txt = "H\te\tl\tl\to"
+
+x =  txt.expandtabs(2)
+
+print(x)
+
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
+
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 49))
+
+txt = "Company12"
+
+x = txt.isalnum()
+
+print(x)
+
+txt = "Hello, welcome to my world."
+
+x = txt.index("welcome")
+
+print(x)
+
+txt = "CompanyX"
+
+x = txt.isalpha()
+
+print(x)
+
+txt = "My name is Ståle"
+
+print(txt.encode(encoding="ascii",errors="backslashreplace"))
+print(txt.encode(encoding="ascii",errors="ignore"))
+print(txt.encode(encoding="ascii",errors="namereplace"))
+print(txt.encode(encoding="ascii",errors="replace"))
+print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
+
+txt = "Company123"
+
+x = txt.isascii()
+
+print(x)
+
+txt = "1234"
+
+x = txt.isdecimal()
+
+print(x)
+
+a = "\u0030" #unicode for 0
+b = "\u0047" #unicode for G
+
+print(a.isdecimal())
+print(b.isdecimal())
+
+txt = "   "
+
+x = txt.isspace()
+
+print(x)
+
+txt = "   s   "
+
+x = txt.isspace()
+
+print(x)
+
+txt = "Hello, And Welcome To My World!"
+
+x = txt.istitle()
+
+print(x)
+
+a = "HELLO, AND WELCOME TO MY WORLD"
+b = "Hello"
+c = "22 Names"
+d = "This Is %'!?"
+
+print(a.istitle())
+print(b.istitle())
+print(c.istitle())
+print(d.istitle())
+
+txt = "THIS IS NOW!"
+
+x = txt.isupper()
+
+print(x)
+
+a = "Hello World!"
+b = "hello 123"
+c = "MY NAME IS PETER"
+
+print(a.isupper())
+print(b.isupper())
+print(c.isupper())
+
+myTuple = ("John", "Peter", "Vicky")
+
+x = "#".join(myTuple)
+
+print(x)
+
+myDict = {"name": "John", "country": "Norway"}
+mySeparator = "TEST"
+
+x = mySeparator.join(myDict)
+
+print(x)
+
+txt = "banana"
+
+x = txt.ljust(20)
+
+print(x, "is my favorite fruit.")
+
+
+txt = "banana"
+
+x = txt.ljust(20, "O")
+
+print(x)
+
+txt = "Hello my FRIENDS"
+
+x = txt.lower()
+
+print(x)
+
+txt = "     banana     "
+
+x = txt.lstrip()
+
+print("of all fruits", x, "is my favorite")
+
+txt = "Hello Sam!"
+mytable = str.maketrans("S", "P")
+print(txt.translate(mytable))
+
+txt = "Hi Sam!"
+x = "mSa"
+y = "eJo"
+mytable = str.maketrans(x, y)
+print(txt.translate(mytable))
+
+
+
+
 
