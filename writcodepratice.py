38f772e467bd0b6459b551b0ff8092e689bc473d
Temitope okikiolu Bello <temmytopeqa@gmail.com>
2025-02-22 08:05:42 +0100

    stringsmuililine

--- writcodepratice.py
+++ writcodepratice.py
@@ -92,3 +92,13 @@ print('i have sold 25 calm tea so far this week and my waiting for reviews from 
 print("It's alright")
 print("He is called 'Johnny'")
 print('He is called "Johnny"')
+
+a = "Shade is a great babe"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
