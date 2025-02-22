f50320958ac8b988cf72e42f2fd84d87ae54b32b
Temitope okikiolu Bello <temmytopeqa@gmail.com>
2025-02-22 11:12:26 +0100

    Stringmethod

--- Pythnumbers.py
+++ Pythnumbers.py
@@ -326,7 +326,28 @@ y = "eJo"
 mytable = str.maketrans(x, y)
 print(txt.translate(mytable))
 
+txt = "Hello Sam!"
+mytable = str.maketrans("S", "P")
+print(txt.translate(mytable))
 
+txt = "Hi Sam!"
+x = "mSa"
+y = "eJo"
+mytable = str.maketrans(x, y)
+print(txt.translate(mytable))
+
+txt = "Good night Sam!"
+x = "mSa"
+y = "eJo"
+z = "odnght"
+mytable = str.maketrans(x, y, z)
+print(txt.translate(mytable))
+
+txt = "Good night Sam!"
+x = "mSa"
+y = "eJo"
+z = "odnght"
+print(str.maketrans(x, y, z))
 
 
 
