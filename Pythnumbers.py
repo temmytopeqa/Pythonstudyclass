056b7ff1dc811cc970e8e5d321600d7deecd3107
Temitope okikiolu Bello <temmytopeqa@gmail.com>
2025-02-22 09:39:30 +0100

    octhexavalue

--- Pythnumbers.py
+++ Pythnumbers.py
@@ -121,4 +121,12 @@ print(txt)
 txt = "Hello \bWorld!"
 print(txt)
 
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt)
+
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
+
 
